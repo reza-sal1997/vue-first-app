b6ba99331dff4ae372b259fd3bea18c2fa67a527
reza-sal1997 <reza.sal1997@gmail.com>
2025-02-07 23:09:53 +0000

    project basic files

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <active-element
-      :topic-title="activeTopic && activeTopic.title"
-      :text="activeTopic && activeTopic.fullText"
+        :topic-title="activeTopic && activeTopic.title"
+        :text="activeTopic && activeTopic.fullText"
     ></active-element>
     <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
   </div>
@@ -18,15 +18,15 @@ export default {
           title: 'The Basics',
           description: 'Core Vue basics you have to know',
           fullText:
-            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+              'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
         },
         {
           id: 'components',
           title: 'Components',
           description:
-            'Components are a core concept for building Vue UIs and apps',
+              'Components are a core concept for building Vue UIs and apps',
           fullText:
-            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+              'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
         },
       ],
       activeTopic: null,
@@ -44,12 +44,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 section {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 2rem auto;
