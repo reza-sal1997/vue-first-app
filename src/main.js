8107ebe5742c7d23f81e2f5bb013d2db41b18141
reza-sal1997 <reza.sal1997@gmail.com>
2025-02-14 07:52:07 +0000

    starting project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue';
 
 import App from './App.vue';
-import FriendContact from "@/components/FriendContact.vue";
 
-const app = createApp(App);
-
-app.component('friend-contact', FriendContact);
-
-app.mount('#app');
+createApp(App).mount('#app');
